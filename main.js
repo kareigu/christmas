4660a7893aedd05173c87a2bb8d492839877176e
mxr <eetu.kivelae@gmail.com>
2020-09-19 00:43:30 +0300

    finished

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-const app = (require('express'))();
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const fs = require('fs');
 const port = 5500;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-	console.log(`GET request: / ${req.query.yes}`);
-	res.send('yes');
+	const now = Date.now();
+	const curYear = new Date().getFullYear();
+	const christmas = new Date(`12/24/${curYear}`);
+	const days = Math.floor((christmas.getTime() - now) / (1000 * 3600 * 24));
+	const hours = Math.floor((christmas.getTime() - now) / (1000 * 3600 * 24 * 60));
+	ejs.renderFile('./index.ejs', { days, hours }, (err, template) => {
+		if (err)
+			throw err;
+		else
+			res.end(template);
+	});
 	});
 
-app.listen(port);
-console.log(`Running on port ${port}`);
+app.listen(port, err => {
+	if (err)
+		throw err;
+	else
+	console.log(`Running on port ${port}`);
+});
